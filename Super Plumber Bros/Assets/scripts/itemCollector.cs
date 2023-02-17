1fb5b37a7b04a90b58edc53a48d36f6f79d4f06a
mostafaamahmoudd <mostafamahmoud805554@std.mans.edu.eg>
2023-02-17 23:21:32 +0200

    final commit
    THE END

---
+++ Super Plumber Bros/Assets/scripts/itemCollector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class itemCollector : MonoBehaviour
+{
+    private int kiwies = 0;
+
+    [SerializeField] private AudioSource collectingSoundEffect;
+    [SerializeField] private Text kiwisText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("kiwi"))
+        {
+            Destroy(collision.gameObject);
+            ++kiwies;
+            collectingSoundEffect.Play();
+            kiwisText.text = "Kiwis: " + kiwies;
+        }
+    }
+}
